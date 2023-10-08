5035dbe2726b4dfc2198106fb262f9a977c966dc
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-10-08 16:22:51 +0200

    modified db seeding

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Start a Postgres database locally using Docker:
 docker run -p 5432:5432 --name some-postgres -e POSTGRES_PASSWORD=password -e POSTGRES_USER=root -ePOSTGRES_DB=modernmoodledb -d postgres
 ```
   
-Run the nodeJS server in dev mode
+Run the nodeJS server in dev mode:
 ```shell
 npm run dev
 ```
--- src/database/seed/seed.ts
+++ src/database/seed/seed.ts
@@ -4,15 +4,25 @@ export async function seed(db: Knex): Promise<void> {
     // Deletes ALL existing entries
     await db("topics").del();
 
-    // Create topics
-    await db("topics").insert([
-        {
-            title: "maths",
-            description: "goat",
-        },
-        {
-            title: "data structure & algorithms",
-            description: "super",
-        }
-    ]);
+    // Create topic math
+    const mathTopicId = await db("topics").insert({
+        title: "math",
+        description: "goat",
+    }).returning("topic_id");
+    
+    // Add chapter 1 to math topic
+    const mathChapter1Id = await db("chapters").insert({
+        topic_id: mathTopicId[0].topic_id,
+        title: "chapter 1",
+        description: "calculus",
+    }).returning("chapter_id");
+
+    // Add exercise 1 to math chapter 1
+    await db("exercises").insert({
+        chapter_id: mathChapter1Id[0].chapter_id,
+        statement: "1 + 2 = ?",
+        solution: "1 + 2 = 3",
+        difficulty_level: 1,
+    });
+
 }
