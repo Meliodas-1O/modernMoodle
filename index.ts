a55ee217c0982aaaf69cb79733c4b64ba6e8dfc4
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-10-08 12:32:45 +0200

    configured dotenv

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
+import dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
 
+// Read .env file
+dotenv.config()
+
+// Configure express app
 const app: Express = express()
-const port: number = 4500
+const port: number = parseInt(process.env.EXPRESS_APP_PORT || '4500')
 
 app.get('/', (_req: Request, res: Response) => {
     res.send("Hello, world")
