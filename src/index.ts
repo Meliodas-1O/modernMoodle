8be532e7083c8b43893e6ce4a657adadd5830c77
Meliodas-1O <fode.cissokho@uha.fr>
2023-10-08 22:23:57 +0200

    refactor : delete unused function

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,11 @@ import express, { Express } from 'express';
 import { addRoutes as addTopicsRoutes } from './routes/topics';
 import { addRoutes as addChaptersRoutes } from './routes/chapters';
 import { EXPRESS_PORT } from './config/config';
-import db from './database/database';
-import { down, up } from './database/migrations/20231008185239_init';
-import { seed } from './database/seed/seed';
-
 
 // Configure express app
 export const app: Express = express()
 const port: number = EXPRESS_PORT;
-async function setupDB() {
-    await down(db);
-    await up(db);
-}
-// Add routes
+
 addTopicsRoutes()
 addChaptersRoutes()
 
