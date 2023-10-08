317bcd9d6ce0f107bcd449161fa764027637e32d
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-10-08 18:39:31 +0200

    The app only starts when the db is ready

--- src/index.ts
+++ src/index.ts
@@ -5,34 +5,43 @@ import { down, up } from './database/migrations/20231008130412_init';
 import db from './database/database';
 import { seed } from './database/seed/seed';
 
+// Create express app
+export const app: Express = express()
 
 // DEV ONLY, recreate database
 // TODO: handle db migrations better
-async function setup_db() {
+async function setupDB() {
     await down(db);
     await up(db);
 }
 
-setup_db()
+function setupAndRunApp() {
+    // Basic configuration of express app
+    const port: number = config.EXPRESS_CONFIG.port
+
+    // Middleware(s)
+    // Parse requests of Content-Type: application/json
+    app.use(express.json())
+
+    // Add routes
+    addTopicsRoutes()
+
+    // Run the server
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    })
+}
+
+setupDB()
     .then(async () => {
         console.log("Database successfully initialized !");
+
+        // Seed the database with some data
         await seed(db);
         console.log("Seed done");
+
+        // Start the app (express)
+        setupAndRunApp();
+
     })
-    .catch(error => { console.error(`"Oops, something went wrong: ${error}`) })
-
-// Basic configuration of express app
-export const app: Express = express()
-const port: number = config.EXPRESS_CONFIG.port
-
-// Middleware(s)
-// Parse requests of Content-Type: application/json
-app.use(express.json())
-
-// Add routes
-addTopicsRoutes()
-
-// Run the server
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+    .catch(error => { console.error(`Oops, something went wrong: ${error}`) })
