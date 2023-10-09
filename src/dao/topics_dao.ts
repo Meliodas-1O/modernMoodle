1fcf6eef075ecd427d428753282cb2b99d6078cb
Meliodas-1O <fode.cissokho@uha.fr>
2023-10-09 21:32:09 +0200

    issue #1 : (refactor -models) : change class name from TopicsDAO to ITopicDAO

---
+++ src/dao/topics_dao.ts
@@ -0,0 +1,9 @@
+import { ITopic } from "../models/topic";
+
+export interface ITopicDAO {
+    getAll(): Promise<ITopic[]>;
+    getById(id: number): Promise<ITopic | undefined>;
+    delete(id: number): Promise<void>;
+    create(topic: ITopic): Promise<number>;
+    update(id: number, newTopic: ITopic): Promise<ITopic | undefined>;
+}
