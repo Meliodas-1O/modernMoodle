61339ae26d15e33f2c21975b5be927b4cd7e0201
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-10-12 21:46:35 +0200

    issue #6 (fix - test style): applied eslint rules to tests

--- test/integration_tests/main_it.integration.test.ts
+++ test/integration_tests/main_it.integration.test.ts
@@ -3,30 +3,30 @@ import request from "supertest";
 import { app } from "../../src/index";
 import { ITopic } from "../../src/models/topic";
 
-describe("Main integration tests suite", () => {
+describe ("Main integration tests suite", () => {
 
-    jest.setTimeout(60 * 1000);
+    jest.setTimeout (60 * 1000);
 
-    beforeAll(async () => {
-        await setup();
+    beforeAll (async () => {
+        await setup ();
     })
 
-    afterAll(async () => {
-        await teardown();
+    afterAll (async () => {
+        await teardown ();
     })
 
-    describe("Topics routes", () => {
+    describe ("Topics routes", () => {
 
-        it("1 - Get all topics", async () => {
+        it ("1 - Get all topics", async () => {
             // Given
             // When
-            const response = await request(app).get("/topics");
+            const response = await request (app).get ("/topics");
 
             // Then
-            expect(response.statusCode).toBe(404);
+            expect (response.statusCode).toBe (404);
         })
 
-        it("2 - Create a new topic", async () => {
+        it ("2 - Create a new topic", async () => {
             // Given
             const newTopic = {
                 title: "topic",
@@ -34,30 +34,30 @@ describe("Main integration tests suite", () => {
             };
 
             // When
-            const response = await request(app)
-                .post("/topics")
-                .send(newTopic)
-                .set("Content-Type", "application/json");
+            const response = await request (app)
+                .post ("/topics")
+                .send (newTopic)
+                .set ("Content-Type", "application/json");
 
             // Then
-            expect(response.statusCode).toBe(200);
+            expect (response.statusCode).toBe (200);
         })
 
-        it("3- Get all topics again", async () => {
+        it ("3- Get all topics again", async () => {
             // Given
             // When
-            const response = await request(app).get("/topics");
+            const response = await request (app).get ("/topics");
 
             // Then
-            expect(response.statusCode).toBe(200);
+            expect (response.statusCode).toBe (200);
 
             const topics: ITopic[] = response.body;
-            console.log(topics);
-            expect(topics.length).toBe(1);
+            console.log (topics);
+            expect (topics.length).toBe (1);
 
             const topic = topics[0];
-            expect(topic.title).toBe("topic");
-            expect(topic.description).toBe("description");
+            expect (topic.title).toBe ("topic");
+            expect (topic.description).toBe ("description");
 
             // TODO(test, fix): the field is called 'topic_id' in the db
             // expect(topic.id).not.toBeUndefined();
--- test/integration_tests/utils/setup.ts
+++ test/integration_tests/utils/setup.ts
@@ -5,46 +5,46 @@ import db_config from "../../../src/database/knexfile";
 import knex from 'knex';
 
 
-const db = knex(db_config.development);
+const db = knex (db_config.development);
 let pg: StartedTestContainer | null = null;
 
 async function startPostgresContainer() {
-    const container = new GenericContainer("postgres:16")
-        .withEnvironment({
+    const container = new GenericContainer ("postgres:16")
+        .withEnvironment ({
             "POSTGRES_PASSWORD": config.PG_CONFIG.password,
             "POSTGRES_USER": config.PG_CONFIG.user,
             "POSTGRES_DB": config.PG_CONFIG.database,
         })
-        .withExposedPorts({ container: config.PG_CONFIG.port, host: config.PG_CONFIG.port });
-    pg = await container.start();
+        .withExposedPorts ({ container: config.PG_CONFIG.port, host: config.PG_CONFIG.port });
+    pg = await container.start ();
 }
 
 async function stopPostgresContainer() {
     if (pg !== null) {
-        await pg.stop();
+        await pg.stop ();
     }
 }
 
 async function sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
+    return new Promise ((resolve) => {
+        setTimeout (resolve, ms);
     });
 }
 
 async function createDatabaseSchema() {
-    await up(db);
+    await up (db);
 }
 
 export async function setup() {
-    await startPostgresContainer();
+    await startPostgresContainer ();
 
     // Wait to be sure that Postgres is ready
-    await sleep(5 * 1000);
+    await sleep (5 * 1000);
 
     // Create the schema
-    await createDatabaseSchema();
+    await createDatabaseSchema ();
 }
 
 export async function teardown() {
-    await stopPostgresContainer();
+    await stopPostgresContainer ();
 }
