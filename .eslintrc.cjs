8a576ca7e36f73acbc7e15c2a0373e0a5a242ad9
Meliodas-1O <fode.cissokho@uha.fr>
2023-10-12 15:46:03 +0200

    issue #10 (fix - api) : fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,6 @@ module.exports = {
     extends: [
       'eslint:recommended', 
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
   ],
     parser: '@typescript-eslint/parser',
     parserOptions: { // add this object
