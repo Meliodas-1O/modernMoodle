50ce43c031d09d1374adeeb78db8553a7f1a040b
Meliodas-1O <fode.cissokho@uha.fr>
2023-10-13 23:46:20 +0200

    issue #6 (feat - test): apply prettier to test files

--- test/integration_tests/utils/setup.ts
+++ test/integration_tests/utils/setup.ts
@@ -1,52 +1,54 @@
-import { GenericContainer, StartedTestContainer } from 'testcontainers';
-import { config } from '../../../src/config/config';
+import { GenericContainer, StartedTestContainer } from "testcontainers";
+import { config } from "../../../src/config/config";
 import { up } from "../../../src/database/migrations/20231008185239_init";
 import db_config from "../../../src/database/knexfile";
-import knex from 'knex';
-import {closeServer } from '../../../src';
+import knex from "knex";
+import { closeServer } from "../../../src";
 
-
-const db = knex (db_config.development);
+const db = knex(db_config.development);
 let pg: StartedTestContainer | null = null;
 
 async function startPostgresContainer() {
-    const container = new GenericContainer ("postgres:16")
-        .withEnvironment ({
-            "POSTGRES_PASSWORD": config.PG_CONFIG.password,
-            "POSTGRES_USER": config.PG_CONFIG.user,
-            "POSTGRES_DB": config.PG_CONFIG.database,
-        })
-        .withExposedPorts ({ container: config.PG_CONFIG.port, host: config.PG_CONFIG.port });
-    pg = await container.start ();
+     const container = new GenericContainer("postgres:16")
+          .withEnvironment({
+               POSTGRES_PASSWORD: config.PG_CONFIG.password,
+               POSTGRES_USER: config.PG_CONFIG.user,
+               POSTGRES_DB: config.PG_CONFIG.database,
+          })
+          .withExposedPorts({
+               container: config.PG_CONFIG.port,
+               host: config.PG_CONFIG.port,
+          });
+     pg = await container.start();
 }
 
 async function stopPostgresContainer() {
-    if (pg !== null) {
-        await pg.stop ();
-    }
+     if (pg !== null) {
+          await pg.stop();
+     }
 }
 
 async function sleep(ms: number): Promise<void> {
-    return new Promise ((resolve) => {
-        setTimeout (resolve, ms);
-    });
+     return new Promise((resolve) => {
+          setTimeout(resolve, ms);
+     });
 }
 
 async function createDatabaseSchema() {
-    await up (db);
+     await up(db);
 }
 
 export async function setup() {
-    await startPostgresContainer ();
+     await startPostgresContainer();
 
-    // Wait to be sure that Postgres is ready
-    await sleep (5 * 1000);
+     // Wait to be sure that Postgres is ready
+     await sleep(3 * 1000);
 
-    // Create the schema
-    await createDatabaseSchema ();
+     // Create the schema
+     await createDatabaseSchema();
 }
 
 export async function teardown() {
-    closeServer();
-    await stopPostgresContainer ();
+     closeServer();
+     await stopPostgresContainer();
 }
