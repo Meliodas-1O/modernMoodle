eae2ed9da0c292d94c37a61a65b231dde92e2f7f
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-10-14 13:30:14 +0200

    issue #6 (feat - di): controllers are singleton

--- src/di/dependency_injection.ts
+++ src/di/dependency_injection.ts
@@ -11,7 +11,15 @@ import { TopicsService } from "../services/impl/topics_service.impl";
 // For now, we create objects "manually".
 // TODO: use a specialized container
 
+let topics_controller: TopicsController | null = null;
+let chapters_controller: ChaptersController | null = null;
+let exercises_controller: ExercisesController | null = null;
+
 export function createTopicsController(): TopicsController {
+     if (topics_controller !== null) {
+          return topics_controller;
+     }
+
      // First, create a PostgresDAO
      const postgres_topics_dao = new PostgresTopicsDAO();
 
@@ -19,11 +27,15 @@ export function createTopicsController(): TopicsController {
      const topics_service = new TopicsService(postgres_topics_dao);
 
      // Finally, create the TopicsController
-     const topics_controller = new TopicsController(topics_service);
+     topics_controller = new TopicsController(topics_service);
      return topics_controller;
 }
 
 export function createChaptersController(): ChaptersController {
+     if (chapters_controller !== null) {
+          return chapters_controller;
+     }
+
      // First, create a PostgresDAO
      const postgres_chapters_dao = new PostgresChapterDAO();
 
@@ -31,11 +43,15 @@ export function createChaptersController(): ChaptersController {
      const chapters_service = new ChaptersService(postgres_chapters_dao);
 
      // Finally, create the ChaptersController
-     const chapters_controller = new ChaptersController(chapters_service);
+     chapters_controller = new ChaptersController(chapters_service);
      return chapters_controller;
 }
 
 export function createExercisesController(): ExercisesController {
+     if (exercises_controller !== null) {
+          return exercises_controller;
+     }
+
      // First, create a PostgresDAO
      const postgres_exercises_dao = new PostgresExerciseDAO();
 
@@ -43,6 +59,6 @@ export function createExercisesController(): ExercisesController {
      const exercises_service = new ExercisesService(postgres_exercises_dao);
 
      // Finally, create the ExercisesController
-     const exercises_controller = new ExercisesController(exercises_service);
+     exercises_controller = new ExercisesController(exercises_service);
      return exercises_controller;
 }
