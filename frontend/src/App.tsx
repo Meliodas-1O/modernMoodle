9f2a66192a4f252b95037f6d0d6bf779691257e5
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-10-15 11:04:09 +0200

    issue #21 (refactor - frontend): applied prettier rules

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, world</h1>
-    </div>
-  );
+     return (
+          <div className="App">
+               <h1>Hello, world</h1>
+          </div>
+     );
 }
 
 export default App;
