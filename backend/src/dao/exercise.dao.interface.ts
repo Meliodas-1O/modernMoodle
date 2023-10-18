e390957b0ed9ed4cde53a569e18a8ba5198dc11c
Meliodas-1O <fode.cissokho@uha.fr>
2023-10-18 08:02:37 +0200

    issue #25 (feat - api) : add ad undefined return type to delete function

--- backend/src/dao/exercise.dao.interface.ts
+++ backend/src/dao/exercise.dao.interface.ts
@@ -3,7 +3,7 @@ import { IExercise } from "../models/exercise";
 export interface IExerciseDAO {
      getAll(): Promise<IExercise[]>;
      getById(id: number): Promise<IExercise | undefined>;
-     delete(id: number): Promise<void>;
-     create(topic: IExercise): Promise<number>;
+     delete(id: number): Promise<number | undefined>;
+     create(topic: IExercise): Promise<number | undefined>;
      update(id: number, newExercise: IExercise): Promise<IExercise | undefined>;
 }
