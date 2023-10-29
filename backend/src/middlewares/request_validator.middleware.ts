5c75658b83b5d6a279b24c85d4b37c253dab68ca
Meliodas-1O <fode.cissokho@uha.fr>
2023-10-29 22:43:53 +0100

    issue #42 (feat - api) : add joi middlewares in topic routes

--- backend/src/middlewares/request_validator.middleware.ts
+++ backend/src/middlewares/request_validator.middleware.ts
@@ -3,41 +3,77 @@ import Joi from "joi";
 
 // -- Chapters section start
 const chaptersGetAllSchema = Joi.object({
-    topicId: Joi.number().optional(),
+     topicId: Joi.number().optional(),
 });
 
 const chaptersGetById = Joi.object({
-    id: Joi.number().required(),
+     id: Joi.number().required(),
 });
 
 const chaptersCreate = Joi.object({
-    topicId: Joi.number().required(),
-    title: Joi.string().required(),
-    description: Joi.string().required(),
+     topicId: Joi.number().required(),
+     title: Joi.string().required(),
+     description: Joi.string().required(),
 });
 
 const chaptersDelete = Joi.object({
-    id: Joi.number().required(),
+     id: Joi.number().required(),
 });
 
 const chaptersUpdateParams = Joi.object({
-    id: Joi.number().required(),
+     id: Joi.number().required(),
 });
 
 const chaptersUpdateBody = Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
+     title: Joi.string().required(),
+     description: Joi.string().required(),
 });
 // -- Chapters section end
 
+// -- Topics section start
+
+const topicCreationSchema = Joi.object({
+     title: Joi.string()
+          .required()
+          .regex(/^[a-zA-Z0-9]+$/),
+     description: Joi.string()
+          .required()
+          .regex(/^[a-zA-Z0-9]+$/),
+});
+
+const topicGetByIdSchema = Joi.object({
+     id: Joi.number().required(),
+});
+
+const topicUpdateBodySchema = Joi.object({
+     title: Joi.string().regex(/^[a-zA-Z0-9]+$/),
+     description: Joi.string().regex(/^[a-zA-Z0-9]+$/),
+}).or("title", "description");
+
+const topicUpdateParamSchema = Joi.object({
+     id: Joi.number().required(),
+});
+
+const topicDeleteSchema = Joi.object({
+     id: Joi.number().required(),
+});
+// -- Topics section end
+
 // Export validator
 export const validatorMiddleware = {
-    chapter: {
-        chaptersGetAllSchema,
-        chaptersGetById,
-        chaptersCreate,
-        chaptersDelete,
-        chaptersUpdateParams,
-        chaptersUpdateBody,
-    }
+     chapter: {
+          chaptersGetAllSchema,
+          chaptersGetById,
+          chaptersCreate,
+          chaptersDelete,
+          chaptersUpdateParams,
+          chaptersUpdateBody,
+     },
+     topic: {
+          topicCreationSchema,
+          topicGetByIdSchema,
+          topicUpdateBodySchema,
+          topicUpdateParamSchema,
+          topicDeleteSchema,
+     },
 };
--- backend/src/routes/topics.routes.ts
+++ backend/src/routes/topics.routes.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { createValidator } from "express-joi-validation";
+
 import { createTopicsController } from "../di/dependency_injection";
+import { validatorMiddleware } from "../middlewares/request_validator.middleware";
 
 // GET      /topics -> all topics
 // GET      /topics/:id -> topic with id
@@ -9,11 +12,29 @@ import { createTopicsController } from "../di/dependency_injection";
 
 const controller = createTopicsController();
 const router = express.Router();
+const validator = createValidator();
 
 router.get("/", controller.getAllTopics);
-router.get("/:id", controller.getTopicById);
-router.post("/", controller.createTopic);
-router.patch("/:id", controller.updateTopic);
-router.delete("/:id", controller.deleteTopic);
+router.get(
+     "/:id",
+     validator.params(validatorMiddleware.topic.topicGetByIdSchema),
+     controller.getTopicById
+);
+router.post(
+     "/",
+     validator.body(validatorMiddleware.topic.topicCreationSchema),
+     controller.createTopic
+);
+router.patch(
+     "/:id",
+     validator.params(validatorMiddleware.topic.topicUpdateParamSchema),
+     validator.body(validatorMiddleware.topic.topicUpdateBodySchema),
+     controller.updateTopic
+);
+router.delete(
+     "/:id",
+     validator.params(validatorMiddleware.topic.topicDeleteSchema),
+     controller.deleteTopic
+);
 
 export default router;
